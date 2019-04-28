e68c696b8e35c19ca8324380a0cd03559f6c37c5
George  Pianka <gpianka1@gmail.com>
2019-04-28 05:16:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ function retrieveEmployeeInformation(){
   return document.querySelector('input').value;
 }
 
+function addNewElementAsLi() {
+  let listElement =  document.createElement('li');
 
+   listElement.innerHTML = retrieveEmployeeInformation();
+
+   document.querySelector('.employee-list').appendChild(listElement);
+}
 
 
