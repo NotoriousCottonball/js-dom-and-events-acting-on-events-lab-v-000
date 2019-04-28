c4fd293b4d456df0d3ed3051c1dfaac0e3cd6a28
George  Pianka <gpianka1@gmail.com>
2019-04-28 07:47:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function retrieveEmployeeInformation(input){
 
 
  function addNewLiOnClick(event){
-  let submit = $('form input')[1];
+  let submit = $('input')[1];
   submit.addEventListener('click', event => {
     addNewElementAsLi();
     resetInput();
