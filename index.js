b71c9e6d32b89ecb2d1f2f3a078c6b6f21d9d12f
George  Pianka <gpianka1@gmail.com>
2019-04-28 07:36:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,16 +53,12 @@ function retrieveEmployeeInformation(input){
   $('ul.employee-list').append(retrieveEmployeeInformation());
 }
 
- function clearFormField() {
-  document.getElementById('myform').reset();
-}
 
  function addNewLiOnClick(event){
   let submit = $('form input:last')[0];
   submit.addEventListener('click', event => {
     addNewElementAsLi();
-    clearFormField();
-    return false;
+   resetInput();
   })
 }
 
