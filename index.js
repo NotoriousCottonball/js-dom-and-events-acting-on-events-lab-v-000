62325b5692a09fc80d65bd6cc9492030dce1c6c6
George  Pianka <gpianka1@gmail.com>
2019-04-28 06:40:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,32 +7,28 @@ function preventRefreshOnSubmit(){
 preventRefreshOnSubmit();
 
 
-function retrieveEmployeeInformation(){
-  return document.querySelector('input').value
+function retrieveEmployeeInformation() {
+  return $('input').val()
 }
 
- function addNewElementAsLi() {
-  let listElement =  document.createElement('li');
-
-   listElement.innerHTML = retrieveEmployeeInformation();
-
-   document.querySelector('.employee-list').appendChild(listElement);
+ function addNewElementAsLi(){
+  let newLi = document.createElement('li');
+  newLi.innerHTML = retrieveEmployeeInformation();
+  const list = document.querySelector('.employee-list');
+  list.appendChild(newLi);
 }
 
- function addNewLiOnClick() {
-
-   document.querySelector('form').addEventListener('submit', function(e) {
+ function addNewLiOnClick(){
+  let submit = document.querySelector("input[type = 'submit']");
+  submit.addEventListener('click', function(){
     addNewElementAsLi();
-    document.querySelector('input').value = "";
+    document.querySelector('input').value = '';
+  })
+
+ }
+function clearEmployeeListOnLinkClick(){
+  let clear = document.querySelector('a');
+  clear.addEventListener('click', function(){
+    document.querySelector('.employee-list').innerHTML = '';
   })
 }
-
- function clearEmployeeListOnLinkClick() {
-  document.querySelector('a').addEventListener('click', function() {
-    let list = document.querySelectorAll('.employee-list li')
-
-     for (let i=0; i < list.length; i++) {
-      list[i].remove();
-    }
-  })
-} 
