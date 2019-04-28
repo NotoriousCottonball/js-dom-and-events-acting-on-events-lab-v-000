b95cdf88cbfadf91122616b00dd258578a4f7e28
George  Pianka <gpianka1@gmail.com>
2019-04-28 07:29:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-function preventRefreshOnSubmit(){
-    document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault();
-    });
-}
 
-preventRefreshOnSubmit();
 
 function retrieveEmployeeInformation(){
   return document.querySelector('input[name="name"]').value
